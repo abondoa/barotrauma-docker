cdb55304618f182c2a07607318dcdd6db843a54e
Laura Demkowicz-Duffy <edward70891@gmail.com>
2020-06-06 00:13:24 +0100

    Basic argparsing

--- mod_manager/src/main.rs
+++ mod_manager/src/main.rs
@@ -1,3 +1,24 @@
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "mod_manager", about = "Manage your Barotrauma mods!")]
+enum Opt {
+    Install {
+        #[structopt(name = "USERNAME", about = "The steam user to use to install the mods")]
+        username: String,
+        #[structopt(name = "ID", about = "A list of mod IDs to install")]
+        mods: Vec<u64>,
+    },
+    Remove {
+        #[structopt(name = "ID", about = "A list of mod IDs to remove")]
+        mods: Vec<u64>,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    match opt {
+        Opt::Install { username, mods } => println!("Installing {:?} as {}", mods, username),
+        _ => (),
+    }
 }
